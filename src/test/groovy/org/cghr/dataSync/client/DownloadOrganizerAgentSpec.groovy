628d2590c3633dace7b92c1b58bf18c90c9c85ab
ravitej <ravitejvaranasi@gmail.com>
2014-01-25 09:52:41 +0530

    renamed DownloadOrganizerAgent

--- src/test/groovy/org/cghr/dataSync/client/DownloadOrganizerAgentSpec.groovy
+++ src/test/groovy/org/cghr/dataSync/client/DownloadOrganizerAgentSpec.groovy
@@ -1,11 +1,14 @@
-package org.cghr.dataSync
+package org.cghr.dataSync.client
 
 /**
  * Created with IntelliJ IDEA.
  * User: ravitej
  * Date: 25/1/14
- * Time: 9:27 AM
+ * Time: 9:46 AM
  * To change this template use File | Settings | File Templates.
  */
-class DownloadOrganizerSpec {
+class DownloadOrganizerAgentSpec {
+
+
+
 }
