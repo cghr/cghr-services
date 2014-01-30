9a1375b187169e368f3ccc551f4a8b55c89322b0
ravitez <ravitejvaranasi@gmail.com>
2014-01-30 12:15:33 +0530

    Parameterized all specs

--- src/test/groovy/org/cghr/commons/db/DbAccessSpec.groovy
+++ src/test/groovy/org/cghr/commons/db/DbAccessSpec.groovy
@@ -1,4 +1,5 @@
 package org.cghr.commons.db
+
 import com.google.gson.Gson
 import org.cghr.test.db.DbTester
 import org.cghr.test.db.MockData
@@ -13,12 +14,19 @@ class DbAccessSpec extends Specification {
     //specific
     @Autowired
     DbAccess dbAccess //SUS
+
+    @Shared
     def singleRowSql = 'select * from country where name=?'
+    @Shared
     def validParamsSingleRow = ['india'];
+    @Shared
     def invalidParamsSingleRow = ['dummyContry'];
 
+    @Shared
     def multipleRowSql = 'select * from country where continent=?'
+    @Shared
     def validParamsMultipleRow = ['asia'];
+    @Shared
     def invalidParamsMultipleRow = ['dummyContinent'];
 
 
@@ -39,8 +47,8 @@ class DbAccessSpec extends Specification {
 
 
     }
-    def setup()
-    {
+
+    def setup() {
         dt.cleanInsert("country")
     }
 
@@ -48,87 +56,99 @@ class DbAccessSpec extends Specification {
     def "should have rows for a valid sql and no rows for an invalid sql"() {
 
         expect:
-        dbAccess.hasRows(singleRowSql, validParamsSingleRow) == true
-        dbAccess.hasRows(singleRowSql, invalidParamsSingleRow) == false
+        dbAccess.hasRows(sql, params) == result
 
+
+        where:
+        sql          | params                 || result
+        singleRowSql | validParamsSingleRow   || true
+        singleRowSql | invalidParamsSingleRow || false
 
     }
 
     def "should get database row as a Map object"() {
 
         expect:
-        assert rowMap.equals(dbAccess.getRowAsMap(singleRowSql, validParamsSingleRow))
-        assert emptyMap.equals(dbAccess.getRowAsMap(singleRowSql, invalidParamsSingleRow))
+        dbAccess.getRowAsMap(sql, params) == result
 
         where:
-        rowMap = dataSet[0]
-        emptyMap = [:]
+        sql          | params                 || result
+        singleRowSql | validParamsSingleRow   || dataSet[0]
+        singleRowSql | invalidParamsSingleRow || [:]
     }
 
 
     def "should get db rows as List of Map Objects"() {
         expect:
-        list == dbAccess.getRowsAsListOfMaps(multipleRowSql, validParamsMultipleRow)
-        emptyList == dbAccess.getRowsAsListOfMaps(multipleRowSql, invalidParamsMultipleRow)
+        dbAccess.getRowsAsListOfMaps(sql, params) == result
 
         where:
-        list = dataSet
-        emptyList = []
+        sql            | params                   || result
+        multipleRowSql | validParamsMultipleRow   || dataSet
+        multipleRowSql | invalidParamsMultipleRow || []
+
     }
 
 
     def "should get db row as a Json"() {
 
         expect:
-        jsonString == dbAccess.getRowAsJson(singleRowSql, validParamsSingleRow)
-        emptyJson == dbAccess.getRowAsJson(singleRowSql, invalidParamsSingleRow)
+        dbAccess.getRowAsJson(sql, params) == result
 
         where:
-        //jsonString='{"id":1,"name":"india","continent":"asia"}'
-        jsonString = new Gson().toJson(dataSet[0]).toString()
-        emptyJson = '{}'
+        sql          | params                 || result
+        singleRowSql | validParamsSingleRow   || new Gson().toJson(dataSet[0]).toString()
+        singleRowSql | invalidParamsSingleRow || '{}'
+
     }
 
     def "should get db row as Json from dataStore,key,value"() {
 
+
         expect:
-        jsonString == dbAccess.getRowAsJson(dataStore, 'id', '1')
-        emptyJson == dbAccess.getRowAsJson(dataStore, 'id', '0')
+        dbAccess.getRowAsJson(dataStore, key, value) == result
 
         where:
-        //jsonString='{"id":1,"name":"india","continent":"asia"}'
-        jsonString = new Gson().toJson(dataSet[0]).toString()
-        emptyJson = '{}'
+        key  | value || result
+        'id' | '1'   || new Gson().toJson(dataSet[0]).toString()
+        'id' | '0'   || '{}'
+
 
     }
 
     def "should get db rows as JsonArray of Json Objects"() {
 
         expect:
-        jsonArray == dbAccess.getRowsAsJsonArray(multipleRowSql, validParamsMultipleRow)
-        emptyArray == dbAccess.getRowsAsJsonArray(multipleRowSql, invalidParamsMultipleRow)
+        dbAccess.getRowsAsJsonArray(sql, params) == result
 
         where:
-        jsonArray = new Gson().toJson(dataSet)
-        emptyArray = '[]'
+        sql            | params                   || result
+        multipleRowSql | validParamsMultipleRow   || new Gson().toJson(dataSet)
+        multipleRowSql | invalidParamsMultipleRow || '[]'
+
     }
 
     def "should get db rows as JsonArray of Json Objects With Only Values No Column Names (keys)"() {
 
         expect:
-        jsonArray == dbAccess.getRowsAsJsonArrayOnlyValues(multipleRowSql, validParamsMultipleRow)
-        emptyArray == dbAccess.getRowsAsJsonArrayOnlyValues(multipleRowSql, invalidParamsMultipleRow)
+        dbAccess.getRowsAsJsonArrayOnlyValues(sql, params) == result
 
         where:
-        jsonArray = '[[1,"india","asia"],[2,"pakistan","asia"],[3,"srilanka","asia"]]'
-        emptyArray = '[]'
+        sql            | params                   || result
+        multipleRowSql | validParamsMultipleRow   || '[[1,"india","asia"],[2,"pakistan","asia"],[3,"srilanka","asia"]]'
+        multipleRowSql | invalidParamsMultipleRow || '[]'
+
     }
 
     def "should get  column Labels for an sql"() {
+
         expect:
-        labels == dbAccess.getColumnLabels(singleRowSql, validParamsSingleRow)
+        dbAccess.getColumnLabels(sql, params) == result
+
         where:
-        labels = 'id,name,continent'
+        sql          | params               || result
+        singleRowSql | validParamsSingleRow || 'id,name,continent'
+
     }
 
     def "should remove data for a given table with a given condition"() {
