7a577f4168b5fa48ecf43d1ec5187efd8b218d55
ravitez <ravitejvaranasi@gmail.com>
2014-01-30 22:33:46 +0530

    changed version number

--- src/main/groovy/org/cghr/commons/db/DbAccess.groovy
+++ src/main/groovy/org/cghr/commons/db/DbAccess.groovy
@@ -1,75 +1,75 @@
 package org.cghr.commons.db
 
-import com.google.gson.Gson
 import groovy.sql.Sql
+
+import com.google.gson.Gson
 
 class DbAccess {
 
 
-    Sql gSql;
-    Gson gson = new Gson()
+	Sql gSql;
+	Gson gson = new Gson()
 
-    DbAccess(Sql gSql) {
-        this.gSql = gSql
+	DbAccess(Sql gSql) {
+		this.gSql = gSql
+	}
 
-    }
+	boolean hasRows(String sql, List params) {
 
-    boolean hasRows(String sql, List params) {
+		def row = gSql.firstRow("select count(*) count from (${sql}) a", params)
+		row.count > 0
+	}
 
-        def row = gSql.firstRow("select count(*) count from (${sql}) a", params)
-        row.count > 0
-    }
+	Map<String, String> getRowAsMap(String sql, List params) {
 
-    Map<String, String> getRowAsMap(String sql, List params) {
+		hasRows(sql, params) ? gSql.firstRow(sql, params) : [:] //empty map
 
-        hasRows(sql, params) ? gSql.firstRow(sql, params) : [:] //empty map
+	}
 
-    }
+	List getRowsAsListOfMaps(String sql, List params) {
 
-    List getRowsAsListOfMaps(String sql, List params) {
+		hasRows(sql, params) ? gSql.rows(sql, params) : [] //empty list
+	}
 
-        hasRows(sql, params) ? gSql.rows(sql, params) : [] //empty list
-    }
+	String getRowAsJson(String sql, List params) {
 
-    String getRowAsJson(String sql, List params) {
+		def rows = gSql.rows(sql, params)
+		rows.isEmpty() ? '{}' : gson.toJson(rows[0])
+	}
+	//overloaded
+	String getRowAsJson(String dataStore, String keyField, String keyFieldValue) {
+		def sql = "select * from $dataStore where $keyField=$keyFieldValue"
+		getRowAsJson(sql, [])
+	}
 
-        def rows = gSql.rows(sql, params)
-        rows.isEmpty() ? '{}' : gson.toJson(rows[0])
-    }
-    //overloaded
-    String getRowAsJson(String dataStore, String keyField, String keyFieldValue) {
-        def sql = "select * from $dataStore where $keyField=$keyFieldValue"
-        getRowAsJson(sql, [])
-    }
+	String getRowsAsJsonArray(String sql, List params) {
 
-    String getRowsAsJsonArray(String sql, List params) {
+		def rows = gSql.rows(sql, params)
+		rows.isEmpty() ? '[]' : gson.toJson(rows)
+	}
 
-        def rows = gSql.rows(sql, params)
-        rows.isEmpty() ? '[]' : gson.toJson(rows)
-    }
+	String getRowsAsJsonArrayOnlyValues(String sql, List params) {
 
-    String getRowsAsJsonArrayOnlyValues(String sql, List params) {
+		def rows = gSql.rows(sql, params)
+		rows.isEmpty() ? '[]' : gson.toJson(rows.collect() { row -> row.values() })
+	}
 
-        def rows = gSql.rows(sql, params)
-        rows.isEmpty() ? '[]' : gson.toJson(rows.collect() { row -> row.values() })
-    }
+	String getColumnLabels(String sql, List params) {
+		List columnLabels = []
+		def metaClosure = { meta ->
+			(1..meta.columnCount).each {
+				columnLabels.add(meta.getColumnLabel(it))
+			}
+		}
 
-    String getColumnLabels(String sql, List params) {
-        List columnLabels = []
-        def metaClosure = { meta ->
-            (1..meta.columnCount).each {
-                columnLabels.add(meta.getColumnLabel(it))
-            }
-        }
+		gSql.rows(sql, params, metaClosure)
+		columnLabels.join(",")
+	}
 
-        gSql.rows(sql, params, metaClosure)
-        columnLabels.join(",")
-    }
+	void removeData(String table, String keyField, Object value) {
+		def sql = "delete from $table where $keyField=?".toString()
+		gSql.executeUpdate(sql, [value])
 
-    void removeData(String table, String keyField, Object value) {
-        def sql = "delete from $table where $keyField=?".toString()
-        gSql.executeUpdate(sql, [value])
-
-    }
+	}
 
 }
