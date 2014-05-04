b4d18cc043beea3ae19d2d2834fe255e400201a2
ravitej <ravitejvaranasi@gmail.com>
2014-05-04 18:42:22 +0530

    chore(dataSync):Agent Provider

--- src/main/groovy/org/cghr/dataSync/commons/AgentProvider.groovy
+++ src/main/groovy/org/cghr/dataSync/commons/AgentProvider.groovy
@@ -3,6 +3,10 @@ package org.cghr.dataSync.commons
 import groovy.sql.Sql
 import org.cghr.commons.db.DbAccess
 import org.cghr.commons.db.DbStore
+import org.cghr.dataSync.client.DownloadAgent
+import org.cghr.dataSync.client.DownloadOrganizerAgent
+import org.cghr.dataSync.client.UploadAgent
+import org.cghr.dataSync.service.AgentService
 import org.springframework.web.client.RestTemplate
 
 class AgentProvider {
@@ -18,9 +22,53 @@ class AgentProvider {
     String downloadDataBatchPath
     String uploadPath
 
-    static Agent provide(String agentId) {
+    //Dynamic Properties
+    String syncServerDownloadInfoUrl
+    String syncServerUploadUrl
+    String syncServerDownloadDataBatchUrl
+    AgentService agentService
 
-        return null
+    //Agents
+    Agent downloadOrganizerAgent
+    Agent downloadAgent
+    Agent msgDistAgent
+    Agent uploadAgent
+
+
+    List<Agent> provideAllAgents(String agentId) {
+
+        createAgentsDynamically()
+        return [downloadOrganizerAgent, downloadAgent, msgDistAgent, uploadAgent]
+
+    }
+
+    void createAgentsDynamically() {
+
+        createDynamicSyncServerUrls()
+        createAgentService()
+        downloadOrganizerAgent = new DownloadOrganizerAgent(agentService)
+        downloadAgent = new DownloadAgent(agentService)
+        msgDistAgent = new MsgDistAgent(agentService)
+        uploadAgent = new UploadAgent(agentService)
+
+    }
+
+    void createAgentService() {
+        this.agentService = new AgentService(gSql, dbAccess, dbStore, syncServerDownloadInfoUrl, syncServerUploadUrl, restTemplate, changelogChunkSize, syncServerDownloadDataBatchUrl)
+
+    }
+
+    void createDynamicSyncServerUrls() {
+
+        String syncServer = syncServerBaseUrl()
+        syncServerDownloadInfoUrl = syncServer + downloadInfoPath
+        syncServerUploadUrl = syncServer + uploadPath
+        syncServerDownloadDataBatchUrl = syncServer + downloadDataBatchPath
+
+
+    }
+    //Todo
+    String syncServerBaseUrl() {
 
     }
 
